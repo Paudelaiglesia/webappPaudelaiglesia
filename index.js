e00f9da6fc725421069f2eabb0f1c4967896ffdb
Paudelaiglesia <pau.delaiglesia@institutroquetes.cat>
2025-04-03 09:44:37 +0200

    inicia sessió js

--- index.js
+++ index.js
@@ -17,3 +17,25 @@ function canvia_seccio(num_boto) {
         }
     }
 }
+let validat = false;    // variable que permet saber si hi ha algun usuari validat
+let nom, contrasenya;
+let scriptURL = "https://script.google.com/macros/s/AKfycbzvBuAUpE6oqCnFAdmjfzB59wvfTGa5UPuphkkANaelo_OcW_NIY-f-f1wI_ZAIUrpTEQ/exec"    // s'ha de substituir la cadena de text per la URL del script
+
+function inici_sessio() {
+    nom = document.getElementById("nom_usuari").value;    // la propietat "value" d'un quadre de text correspon al text escrit per l'usuari
+    contrasenya = document.getElementById("contrasenya").value;
+    let consulta = scriptURL + "?query=select&where=usuari&is=" + nom + "&and=contrasenya&equal=" + contrasenya;
+    fetch(consulta)
+        .then((resposta) => {   // registres que contenen el nom d'usuari i contrasenya escrits per l'usuari
+            return resposta.json();    // conversió a llista
+        })
+        .then((resposta) => {
+            if(resposta.length == 0) {    // llista buida
+                window.alert("El nom d'usuari o la contrasenya no són correctes.");
+            }
+            else {    // llista amb (almenys) un registre
+                window.alert("S'ha iniciat correctament la sessió.");
+                inicia_sessio();    // usuari validat, s'executen les instruccions del procediment "inicia_sessio"
+            }
+        });    
+}

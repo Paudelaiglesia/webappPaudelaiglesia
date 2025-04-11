046b93f6a0d5825dfc0672ebde42119d81ebe28e
Paudelaiglesia <pau.delaiglesia@institutroquetes.cat>
2025-04-11 12:03:54 +0200

    canvi

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ function canvia_seccio(num_boto) {
             seccio.style.display = "none";    // s'oculten les seccions inactives
         }
     }
+}
 function inici_sessio() {
     nom = document.getElementById("nom_usuari").value;    // la propietat "value" d'un quadre de text correspon al text escrit per l'usuari
     contrasenya = document.getElementById("contrasenya").value;
@@ -100,11 +101,7 @@ window.onload = () => {
             }
         }
     });
-    mapa = L.map("seccio_4").setView([41.72, 1.82], 8);    // assigna el mapa a la secció, centrat en el punt i amb el nivell de zoom
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {    // capa d'OpenStreetMap
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'    // autoria de la capa
-    }).addTo(mapa);    // s'afegeix la capa al mapa
-}
+}    
 function desa_foto() {
     let nou_registre = {    // contingut del nou registre de la base de dades
         Usuari: usuari,    // nom d'usuari

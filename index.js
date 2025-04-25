3456bc87fd772c87b235fea945b9dcaf07ff8c3a
Paudelaiglesia <pau.delaiglesia@institutroquetes.cat>
2025-04-25 12:07:17 +0200

    NUM_BOTO 6

--- index.js
+++ index.js
@@ -27,6 +27,9 @@ function canvia_seccio(num_boto) {
                 navigator.geolocation.watchPosition(geoExit);    // inicia el seguiment de la localització del dispositiu
             }
         }
+        if (num_boto == 6) {
+            mostra_diagrama();
+        }
     }
 }
 function inici_sessio() {
